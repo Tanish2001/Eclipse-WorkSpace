31f86a6659350bf4ff348a617cd77e1414ef2b6c
Tanish Batham <Tanish.Batham@asl.mum.sst>
2023-07-12 19:54:39 +0530

    ok

---
+++ 11-account-withdraw-deposite/src/com/aurionpro/test/AccountTest.java
@@ -0,0 +1,23 @@
+package com.aurionpro.test;
+
+import com.aurionpro.model.Account;
+import com.aurionpro.model.AccountType;
+
+public class AccountTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Account account = new Account();
+		account.setBalance(100);
+		account.setId(1);
+		account.setName("Ban");
+		account.setType(AccountType.SAVINGS);
+		
+		System.out.println(account);
+	}
+	
+	 private static void deposite(double amount) {
+		 double updatedBalance += ;
+	 }
+
+}
