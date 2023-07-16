9cc6da44683d722402357455f03fcd9b2e0289e1
Tanish Batham <Tanish.Batham@asl.mum.sst>
2023-07-16 15:31:34 +0530

    16/07/23

---
+++ 22-MatrixMultiplication/src/com/aurionpro/test/MatrixMultiply.java
@@ -0,0 +1,37 @@
+package com.aurionpro.test;
+public class MatrixMultiply {
+
+	public static void main(String[] args) {
+		int count=0;
+		int[][] matrix1 = {
+				{1,2,5},
+				{2,3,5},
+				{4,7,1}
+		};
+		int[][] matrix2 = {
+				{1,2,5},
+				{2,3,5},
+				{4,7,1}
+		};
+		
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                int sum = 0;
+                for (int k = 0; k < 3; k++) {
+                    sum += matrix1[i][k] * matrix2[k][j];
+
+                }
+                if(count==3) {
+                	System.out.println("\n");
+                	count=0;
+            }
+                count++;
+                System.out.print(" "+sum);
+
+			}
+		}
+		
+		
+	}
+
+}
