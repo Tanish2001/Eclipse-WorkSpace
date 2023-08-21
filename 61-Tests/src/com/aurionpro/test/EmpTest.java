f686f7b58083a635dc8f2465d54892a037e38ea7
Tanish2001 <bathamtanish@gmail.com>
2023-08-21 13:49:20 +0530

    21/08/2023

---
+++ 61-Tests/src/com/aurionpro/test/EmpTest.java
@@ -0,0 +1,27 @@
+package com.aurionpro.test;
+
+import com.aurionpro.model.EmployeeManagement;
+
+public class EmpTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EmployeeManagement man = new EmployeeManagement();
+//		man.convertToEmpObj();
+//		man.serializeData();
+//		man.loadData();
+//		man.deleteEmp();
+//		man.getAllEmp();
+//		man.getAllFromDepartment();
+//		man.countOfEmpInEachDepartment();
+//		man.addNewEmp();
+//		man.getManagerOfEmp();
+//		man.highestPiad();
+//		man.highestPaidForEachRole();
+//		man.highestPaidForEachDepartment();
+//		man.salesmanWithHighComm();
+		man.newJoineesInDate();
+		man.sumOfAllSalaries();
+	}
+
+}
